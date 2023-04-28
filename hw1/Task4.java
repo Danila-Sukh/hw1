67ed4c3d70f2c496988990c063274b84120a7f72
Danila Sukhorukov <suhorukovdanila@yandex.ru>
2023-04-28 19:22:11 +0300

    first commit

---
+++ hw1/Task4.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class Task4 {
+    public static void main(String[] args) {
+        try {
+            int[] arr1 = {2, 4, 6, 8};
+            int[] arr2 = {1, 3, 5, 7};
+            int[] arr3 = {1, 3, 5, 7, 9};
+
+            System.out.println("Ответ 1: " + Arrays.toString(arrayDifference(arr1, arr2)) + " (Нет исключений)");
+            System.out.println(Arrays.toString(arrayDifference(arr1, arr3)));
+        } catch (RuntimeException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    private static float[] arrayDifference(int[] array1, int[] array2) {
+        if (array1.length != array2.length) {
+            throw new RuntimeException("Размеры массивов не равны");
+        }
+        float[] temp = new float[array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            temp[i] = (float) array1[i] / array2[i];
+        }
+        return temp;
+    }
+}
+
